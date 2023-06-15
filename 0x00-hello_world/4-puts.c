e64a6e84df5a8c8f7dc21fa23730214d0fe2fbda
Dohaibrahim106 <doha.ibrahim106@gmail.com>
2023-06-15 15:27:44 -0700

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
-
 /**
  * main - This is the entry point of the project
  *
  * Return: always 0
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
