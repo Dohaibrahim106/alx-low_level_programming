cfdac0704bd42159f5017891ac8998b7bfcd86e3
Dohaibrahim106 <doha.ibrahim106@gmail.com>
2023-06-20 15:07:59 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int d;
+	for (d = 0; d < 8; d++)
+	_putchar('\n');
+
+return (0);
+}
